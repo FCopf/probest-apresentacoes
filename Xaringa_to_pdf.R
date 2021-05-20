d10cfc6de22bb7aad6e8afbd6ee58045bbe8d2be
FCopf <fabiocopf@gmail.com>
2021-05-20 17:59:22 -0300

    Teste z

---
+++ Xaringa_to_pdf.R
@@ -0,0 +1,8 @@
+#install.packages(c("pagedown", "xaringan"))
+# make sure you have pagedown >= 0.2 and xaringan >= 0.9; if not, run
+# remotes::install_github(c('rstudio/pagedown', 'yihui/xaringan'))
+
+pagedown::chrome_print("Teste_Hipoteses.Rmd")
+
+# or just pass the HTML output file path to chrome_print()
+pagedown::chrome_print("Teste_Hipoteses.html")
